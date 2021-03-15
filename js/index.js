c3f81054ea4d17369a0701568f0008c2f9f6c84d
IrinaPopova99 <janneteldenorr@gmail.com>
2021-03-15 15:25:48 +0700

    Add bootstrap

---
+++ js/index.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+// function knowMore(buttonClass, overlayClass, popupCloseClass, animationClass) {
+//     let more = document.querySelectorAll(buttonClass),
+//         overlay = document.querySelector(overlayClass),
+//         close = document.querySelector(popupCloseClass);
+//      more.forEach(more => {       
+//         more.addEventListener('click', () => {
+//             overlay.style.display = 'block';
+//             this.classList.add(animationClass);
+//             document.body.style.overflow = 'hidden';
+//         });
+//      });
+
+//     close.addEventListener('click', () => {
+//         overlay.style.display = 'none';
+//         more.classList.remove(animationClass);
+//         document.body.style.overflow = '';
+//     });
+// }
+// knowMore()
+// });
+
+// let more = document.querySelector('.more'),
+// overlay = document.querySelector('.overlay'),
+// close = document.querySelector('.popup-close');
+
+// more.addEventListener('click', function() {
+// overlay.style.display = 'block';
+// this.classList.add('more-splash');
+// document.body.style.overflow = 'hidden';
+// });
+
+// close.addEventListener('click', function() {
+// overlay.style.display = 'none';
+// more.classList.remove('more-splash');
+// document.body.style.overflow = '';
+
+
+});
